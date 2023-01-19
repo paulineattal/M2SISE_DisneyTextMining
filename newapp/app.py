fa94044691392a2f1d7519258f70799ebb6392a4
christelle cornu <109891813+cornuch@users.noreply.github.com>
2023-01-19 18:40:25 +0100

    Add files via upload

--- newapp/app.py
+++ newapp/app.py
@@ -6,13 +6,13 @@ import gunicorn
 import base64
 import io
 
-#définition de l'application
+#définition de l'application avec plusieurs pages (use_pages=True)
 #utilisation des thèmes BOOSTRAP
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 #pour que l'application puisse être déployée sur le net
 server=app.server
 
-#un composant de naviagtion
+#création d'un composant de navigation
 header = dbc.Navbar(
     #définition du contener qui comportera des pages avec les liens
     dbc.Container(
@@ -20,6 +20,7 @@ header = dbc.Navbar(
             dbc.Row([
                 dbc.NavbarToggler(id="navbar-toggler"),
                     dbc.Nav([
+                        #pourchaque page, on créer un lien
                         dbc.NavLink(page["name"], href=page["path"])
                         for page in dash.page_registry.values()
                         #if not page["path"].startswith("/app")
@@ -28,10 +29,14 @@ header = dbc.Navbar(
         ],
         fluid=True,
     ),
+    #couleurs de la barre de navigation
     dark=True,
     color='secondary'
 )
 
+#on met le NavBar dans le layout
+#dash.page_container permet d'avoir un nouveau chemin dans le navigateur
+#celui-ci est en lien avec les intitulé des différentes pages "enregistrées" (register) 
 app.layout = dbc.Container([header, dash.page_container], fluid=False)
 
 if __name__ == '__main__':
